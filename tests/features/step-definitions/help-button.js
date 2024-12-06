2f0908b9735765ef297f04ab4c83f6e0bd9f9c40
Jensju <jens@modulmedia.se>
2024-12-06 10:57:21 +0100

    add tests

---
+++ tests/features/step-definitions/help-button.js
@@ -0,0 +1,8 @@
+import { Given, When, Then } from '@cucumber/cucumber';
+import { expect } from 'chai';
+import { navigateTo, getWhereIAm, getMenuChoiceElement } from './helpers.js'
+
+
+/*
+All steps repeated from the other step-definitions.
+*/
