7dc5c3603e0b33fcda039a1e16709f36caad114f
Jensju <jens@modulmedia.se>
2024-12-15 00:32:06 +0100

    !

--- tests/features/support/world.js
+++ tests/features/support/world.js
@@ -16,5 +16,50 @@ class CustomWorld {
   }
 }
 
+class CustomWorld {
+  constructor() {
+    this.driver = driver;
+  }
+
+  gotoUrl(url) {
+    return this.driver.get(url);
+  }
+
+  getText(element) {
+    return element.getText();
+  }
+
+  runScriptInBrowser(script) {
+    return this.driver.executeScript(script);
+  }
+
+  get(cssSelector) {
+    return this.driver.findElement(By.css(cssSelector));
+  }
+
+  getMany(cssSelector) {
+    return this.driver.findElements(By.css(cssSelector));
+  }
+
+  getByXPath(xPath) {
+    return this.driver.findElement(By.xpath(xPath));
+  }
+
+  getManyByXPath(xPath) {
+    return this.driver.findElements(By.xpath(xPath));
+  }
+
+  getWait(cssSelector, maxTimeToWaitMs = 5000) {
+    return this.driver.wait(
+      until.elementLocated(By.css(cssSelector)), maxTimeToWaitMs);
+  }
+
+  getByXPathWait(xPath, maxTimeToWaitMs = 5000) {
+    return this.driver.wait(
+      until.elementLocated(By.xpath(xPath)), maxTimeToWaitMs);
+  }
+
+}
+
 setDefaultTimeout(timeout);
 setWorldConstructor(CustomWorld);
